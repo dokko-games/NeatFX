46c6aa03df1da136f63f3df41cc14ef29f675bd1
dokko-games <fazedokko@gmail.com>
2025-03-19 18:45:19 +0100

    Change Anchors to use fields instead of hard-coded values

--- src/main/java/com/dokko/win4jui/api/element/Anchors.java
+++ src/main/java/com/dokko/win4jui/api/element/Anchors.java
@@ -5,14 +5,31 @@ import lombok.Getter;
 @Getter
 public enum Anchors {
 
-    TOP_LEFT(0x0101), TOP_CENTER(0x0102), TOP_RIGHT(0x0103), TOP_FIX(0x0104), TOP_SCALED(0x0105),
-    CENTER_LEFT(0x0201), CENTERED(0x0202), CENTER_RIGHT(0x0203), CENTER_FIX(0x0204), CENTER_SCALED(0x0205),
-    BOTTOM_LEFT(0x0301), BOTTOM_CENTER(0x0302), BOTTOM_RIGHT(0x0303), BOTTOM_FIX(0x0304), BOTTOM_SCALED(0x0305),
-    FIX_LEFT(0x0401), FIX_CENTER(0x0402), FIX_RIGHT(0x0403), FIXED(0x0404), FIX_SCALED(0x0405),
-    SCALED_LEFT(0x0501), SCALED_CENTER(0x0502), SCALED_RIGHT(0x0503), SCALED_FIX(0x0504), SCALED(0x0505);
-    private static final int xMask = 0x00FF, yMask = 0xFF00;
-    private static final int yTop = 0x0100, yCENTER = 0x0200, yBottom = 0x0300, yFix = 0x0400, yScale = 0x0500;
-    private static final int xLeft = 0x0001, xCENTER = 0x0002, xRight = 0x0003, xFix = 0x0004, xScale = 0x0005;
+    TOP_LEFT(AnchorMasks.yTop | AnchorMasks.xLeft), TOP_CENTER(AnchorMasks.yTop | AnchorMasks.xCenter),
+    TOP_RIGHT(AnchorMasks.yTop | AnchorMasks.xRight), TOP_FIX(AnchorMasks.yTop | AnchorMasks.xFix),
+    TOP_SCALED(AnchorMasks.yTop | AnchorMasks.xScale),
+
+    CENTER_LEFT(AnchorMasks.yCenter | AnchorMasks.xLeft), CENTERED(AnchorMasks.yCenter | AnchorMasks.xCenter),
+    CENTER_RIGHT(AnchorMasks.yCenter | AnchorMasks.xRight), CENTER_FIX(AnchorMasks.yCenter | AnchorMasks.xFix),
+    CENTER_SCALED(AnchorMasks.yCenter | AnchorMasks.xScale),
+
+    BOTTOM_LEFT(AnchorMasks.yBottom | AnchorMasks.xLeft), BOTTOM_CENTER(AnchorMasks.yBottom | AnchorMasks.xCenter),
+    BOTTOM_RIGHT(AnchorMasks.yBottom | AnchorMasks.xRight), BOTTOM_FIX(AnchorMasks.yBottom | AnchorMasks.xFix),
+    BOTTOM_SCALED(AnchorMasks.yBottom | AnchorMasks.xScale),
+
+    FIX_LEFT(AnchorMasks.yFix | AnchorMasks.xLeft), FIX_CENTER(AnchorMasks.yFix | AnchorMasks.xCenter),
+    FIX_RIGHT(AnchorMasks.yFix | AnchorMasks.xRight), FIXED(AnchorMasks.yFix | AnchorMasks.xFix),
+    FIX_SCALED(AnchorMasks.yFix | AnchorMasks.xScale),
+
+    SCALED_LEFT(AnchorMasks.yScale | AnchorMasks.xLeft), SCALED_CENTER(AnchorMasks.yScale | AnchorMasks.xCenter),
+    SCALED_RIGHT(AnchorMasks.yScale | AnchorMasks.xRight), SCALED_FIX(AnchorMasks.yScale | AnchorMasks.xFix),
+    SCALED(AnchorMasks.yScale | AnchorMasks.xScale);
+
+    private static class AnchorMasks {
+        private static final int xMask = 0x00FF, yMask = 0xFF00;
+        private static final int yTop = 0x0100, yCenter = 0x0200, yBottom = 0x0300, yFix = 0x0400, yScale = 0x0500;
+        private static final int xLeft = 0x0001, xCenter = 0x0002, xRight = 0x0003, xFix = 0x0004, xScale = 0x0005;
+    }
     private final int code;
     Anchors(int code){
         this.code = code;
@@ -20,35 +37,35 @@ public enum Anchors {
     }
 
     public boolean isXLeft() {
-        return (getCode() & xMask) == xLeft;
+        return (getCode() & AnchorMasks.xMask) == AnchorMasks.xLeft;
     }
     public boolean isXRight() {
-        return (getCode() & xMask) == xRight;
+        return (getCode() & AnchorMasks.xMask) == AnchorMasks.xRight;
     }
     public boolean isXCenter() {
-        return (getCode() & xMask) == xCENTER;
+        return (getCode() & AnchorMasks.xMask) == AnchorMasks.xCenter;
     }
     public boolean isXFix() {
-        return (getCode() & xMask) == xFix;
+        return (getCode() & AnchorMasks.xMask) == AnchorMasks.xFix;
     }
     public boolean isXScaled() {
-        return (getCode() & xMask) == xScale;
+        return (getCode() & AnchorMasks.xMask) == AnchorMasks.xScale;
     }
 
     public boolean isYTop() {
-        return (getCode() & yMask) == yTop;
+        return (getCode() & AnchorMasks.yMask) == AnchorMasks.yTop;
     }
     public boolean isYCenter() {
-        return (getCode() & yMask) == yCENTER;
+        return (getCode() & AnchorMasks.yMask) == AnchorMasks.yCenter;
     }
     public boolean isYBottom() {
-        return (getCode() & yMask) == yBottom;
+        return (getCode() & AnchorMasks.yMask) == AnchorMasks.yBottom;
     }
     public boolean isYFix() {
-        return (getCode() & yMask) == yFix;
+        return (getCode() & AnchorMasks.yMask) == AnchorMasks.yFix;
     }
     public boolean isYScaled() {
-        return (getCode() & yMask) == yScale;
+        return (getCode() & AnchorMasks.yMask) == AnchorMasks.yScale;
     }
 
 }
